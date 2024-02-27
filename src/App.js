989fa08247383923a97f60c5aecbad2c80b0e885
pc-1827 <problemcreator1827@gmail.com>
2024-02-27 22:50:59 +0530

    react setup

---
+++ src/App.js
@@ -0,0 +1,49 @@
+function App() {
+  return (
+    <div className="main">
+      <div className="header">
+      <ul className="header-left">
+        <li>
+          About
+        </li>
+        <li>
+          Projects
+        </li>
+        <li>
+          Contact
+        </li>
+        <li className="resume">
+          Resume
+        </li>
+      </ul>
+    </div>
+    <div className="front-page">
+      <div className="name">Full Stack Web Developer Portfolio</div>
+      <div className="description">I write clean, efficient and elegant code for web applications.</div>
+    </div>
+    <div className="projects-heading">
+      My Projects
+    </div>
+    <div className="projects">
+      <div className="project">
+        <div>Project Name</div>
+        <div>This project utlizes multiple technologies generate something which can be used by people all over the internet.</div>
+      </div>
+      <div className="project">
+        <div>Project Name</div>
+        <div>This project utlizes multiple technologies generate something which can be used by people all over the internet.</div>
+      </div>
+      <div className="project">
+        <div>Project Name</div>
+        <div>This project utlizes multiple technologies generate something which can be used by people all over the internet.</div>
+      </div>
+      <div className="project">
+        <div>Project Name</div>
+        <div>This project utlizes multiple technologies generate something which can be used by people all over the internet.</div>
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default App;
